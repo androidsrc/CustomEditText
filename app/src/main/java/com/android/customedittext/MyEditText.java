d8515ccba51df95edf2e690f36644da08d20d48c
guru <cs2guru@gmail.com>
2016-03-21 11:29:15 +0530

    2nd  commit

--- app/src/main/java/com/android/customedittext/MyEditText.java
+++ app/src/main/java/com/android/customedittext/MyEditText.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 
 public class MyEditText extends EditText {
     // testing git
+    //other commit
 
     public MyEditText(Context context) {
         super(context);
